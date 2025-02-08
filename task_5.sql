7f0a75bcd392f86126d947f5227b2c12ac6108fa
ahmedissa <ahmedissa171090@gmail.com>
2025-02-08 23:44:47 +0200

    create my first database update1

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSERT INTO customer (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
+INSERT INTO customer (customer_id, customer_name, email, address) (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
 (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
